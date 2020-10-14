08a8dff024a2a1be3f8d09b217290a5331d4a522
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-10-14 14:23:34 +0200

    Added DataLoader

---
+++ src/main/java/com/bezkoder/springjwt/bootstrap/DataLoader.java
@@ -0,0 +1,37 @@
+package com.bezkoder.springjwt.bootstrap;
+
+import com.bezkoder.springjwt.models.ERole;
+import com.bezkoder.springjwt.models.Role;
+import com.bezkoder.springjwt.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    public DataLoader(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        loadRoles();
+    }
+
+    private void loadRoles() {
+        Role role1 = new Role();
+        role1.setName(ERole.ROLE_USER);
+        roleRepository.save(role1);
+
+        Role role2 = new Role();
+        role2.setName(ERole.ROLE_MODERATOR);
+        roleRepository.save(role2);
+
+        Role role3 = new Role();
+        role3.setName(ERole.ROLE_ADMIN);
+        roleRepository.save(role3);
+    }
+}
